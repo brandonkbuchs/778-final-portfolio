ef01d08faa57fc4a381d5d959d041b4155988ad6
Brandon Buchs <bbuchs@wisc.edu>
2020-08-10 22:56:20 -0400

    Updates

---
+++ precache-manifest.a51d8ba6bf4a58645e2a1635f81c57d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "679e53df8e0d9175516c7215ef56866b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b3dfa99a040fa2507b6",
+    "url": "/static/css/main.16a58bf5.chunk.css"
+  },
+  {
+    "revision": "4242faa3db62e7e92dfe",
+    "url": "/static/js/2.53c6a95f.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/static/js/2.53c6a95f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b3dfa99a040fa2507b6",
+    "url": "/static/js/main.1b228667.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  }
+]);
